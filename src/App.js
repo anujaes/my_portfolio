8526d84148f681d9004732edc6828cafdca96769
anujaes <anuj.aes@gmail.com>
2022-09-27 17:36:54 +0530

    a small bug fixxed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import About from './components/about';
-import Home from './components/home';
-import NavigationBar from './components/navbar/navbar';
-import Resume from './components/resume';
+import About 			from './components/about';
+import Home 			from './components/home';
+import NavigationBar 	from './components/navbar/navbar';
+import Resume 			from './components/resume';
 function App() {
-  return (
-    <div className="App">
-      <NavigationBar />
-      <Home />
-      <About />
-      <Resume />
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavigationBar />
+			<Home />
+			<About />
+			<Resume />
+		</div>
+	);
 }
 
 export default App;
