76ef2f6594b08a4db853cf79dc7167a96f6c3a13
anujaes <anuj.aes@gmail.com>
2022-11-04 22:14:34 +0530

    footer update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -40,7 +40,6 @@ function NavigationBar() {
                         <Nav.Link className='px-4 pb-1 nav-btn' href="#home">Home</Nav.Link>
                         <Nav.Link className='px-4 pb-1 nav-btn' href="#about">About</Nav.Link>
                         <Nav.Link className='px-4 pb-1 nav-btn' href="#resume">Resume</Nav.Link>
-                        {/* <Nav.Link className='px-4 pb-1 nav-btn' href="#services">Services</Nav.Link> */}
                         <Nav.Link className='px-4 pb-1 nav-btn' href="#projects">Project</Nav.Link>
                         <Nav.Link className='px-4 pb-1 nav-btn' href="#contact">Contact</Nav.Link>
                     </Nav>
