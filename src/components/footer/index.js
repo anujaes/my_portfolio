17eb41fa258f900a309cbe584fdd79854150acd8
anujaes <anuj.aes@gmail.com>
2022-11-04 21:29:46 +0530

    footer change

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,61 +1,70 @@
-import React from "react";
-import { Row, Container,Col }       from "react-bootstrap";
-import * as Icon                    from 'react-bootstrap-icons';
 import '../css/footer.css'
+import React                    from "react";
+import { Row, Container, Col }  from "react-bootstrap";
+import * as Icon                from 'react-bootstrap-icons';
+import potrait                  from '../../images/potrait.png'
 
 function Footer(params) {
     const logo = {
-        width:'20px',
-        height:'20px'
+        width: '20px',
+        height: '20px'
     }
     return (
         <footer>
             <Container className="ps-5 pe-5">
                 <Row>
-                    <Col className="footer-col" lg={3} md={6} sm={12}>
-                        <h2 className="footer-heading">About</h2>
-                        <p>
-                        I am an MCA graduate, open for immediate joining as a Full Stack Developer
-                        in an organization that helps me grow and learn. I look forward to joining a
-                        team that moves ahead together, supporting and motivating one another.
-                        </p>
+                    <Col lg={8}>
+                        <Row>
+                            <Col lg={8} className="footer-col">
+                                <h2 className="footer-heading">About</h2>
+                                <p className="pe-3">
+                                    I am an MCA graduate, open for immediate joining as a Full Stack Developer
+                                    in an organization that helps me grow and learn. I look forward to joining a
+                                    team that moves ahead together, supporting and motivating one another.
+                                </p>
+                            </Col>
+                            {/* <Col lg={4} className="footer-col">
+                                <h2 className="footer-heading ms-5">Links</h2>
+                                <ul className="footer-link hover-effect ps-5">
+                                    <li><a href="#home">Home </a></li>
+                                    <li><a href="#about">About </a></li>
+                                    <li><a href="#resume">Resume </a></li>
+                                    <li><a href="#project">Project </a></li>
+                                </ul>
+                            </Col> */}
+                            <Col lg={8} className="footer-col">
+                                <h2 className="footer-heading">Other Profile</h2>
+                                <ul className="footer-link hover-effect ps-0">
+                                    <li><a href="https://github.com/anujaes">https://github.com/anujaes </a></li>
+                                    <li><a href="https://www.hackerrank.com/ANUJ_MCMT">https://www.hackerrank.com/ANUJ_MCMT </a></li>
+                                </ul>
+                            </Col>
+                            <Col lg={4} className="footer-col">
+                                <h2 className="footer-heading ms-5"></h2>
+                                <ul className="footer-link ps-5 pt-5 footer-social-icon">
+                                    <li>
+                                        <a target="_blank" rel="noreferrer" href="https://www.facebook.com/littleanuj/"><Icon.Facebook height={logo.height} width={logo.width} /></a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/anuj.aes/"><Icon.Instagram height={logo.height} width={logo.width} /></a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/anuj-aes"><Icon.Linkedin height={logo.height} width={logo.width} /></a>
+                                    </li>
+                                </ul>
+                            </Col>
+                            <Col lg={12} className="footer-col">
+                                <p className="mt-5 pe-3">
+                                    Disclaimer, this portfolio is designed using ReactJs, React-Bootstrap and React-Bootstrap-Icons.
+                                    And this portfolio template is inspired from 'ColorLib'. Developed By Anuj Kumar Singh.
+                                    <br></br><br></br>
+                                    <em>{'Last updated on 04 November 2022'}</em>
+                                </p>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col className="footer-col" lg={3} md={6} sm={12}>
-                        <h2 className="footer-heading" style={{marginLeft:"32px"}}>Links</h2>
-                        <ul className="footer-link">
-                            <li><a href="#home"><Icon.ChevronRight width={20} height={20} />Home </a></li>
-                            <li><a href="#about"><Icon.ChevronRight width={20} height={20} />About </a></li>
-                            <li><a href="#resume"><Icon.ChevronRight width={20} height={20} />Resume </a></li>
-                            <li><a href="#contact"><Icon.ChevronRight width={20} height={20} />Contact </a></li>
-                        </ul>
-                    </Col>
-                    <Col className="footer-col" lg={3} md={6} sm={12}>
-                        <h2 className="footer-heading" style={{marginLeft:"32px"}}>Social</h2>
-                        <ul className="footer-link">
-                            <li>
-                                <div className="footer-social">
-                                    <a target="_blank" rel="noreferrer" href="https://www.facebook.com/littleanuj/"><Icon.Facebook  height={logo.height} width={logo.width}/></a>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="footer-social">
-                                    <a target="_blank" rel="noreferrer" href="https://www.instagram.com/anuj.aes/"><Icon.Instagram height={logo.height} width={logo.width} /></a>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="footer-social">
-                                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/anuj-aes"><Icon.Linkedin  height={logo.height} width={logo.width}/></a>
-                                </div>
-                            </li>
-                        </ul>
-                    </Col>
-                    <Col className="footer-col" lg={3} md={6} sm={12}>
-                        <h2 className="footer-heading">Disclaimer</h2>
-                        <p>
-                            This Portfolio is designed using ReactJs, React-Bootstrap and React-Bootstrap-Icons.
-                            <br></br>
-                            And this portfolio template is inspired from 'ColorLib'.
-                        </p>
+                    <Col className="footer-image" lg={4}>
+                        <img src={potrait} alt="Anuj's potrait" />
                     </Col>
                 </Row>
             </Container>
--- src/components/resume/atoms/verticalNav.js
+++ src/components/resume/atoms/verticalNav.js
@@ -4,7 +4,7 @@ import          '../../css/verticalNav.css'
 
 function VerticalNav() {
     return (
-        <Nav defaultActiveKey="#academic" className="flex-column vertical-navigation mb-5">
+        <Nav id = "vertical-navigation" defaultActiveKey="#academic" className="flex-column vertical-navigation mb-5">
             <Nav.Link href="#academic" >Education</Nav.Link>
             <Nav.Link href="#experience" >Experience</Nav.Link>
             <Nav.Link href="#skills" >Skills</Nav.Link>
