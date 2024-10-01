506214470867694cc4966c40c41387763defa396
Anuj Kumar Singh <anuj.aes@gmail.com>
2024-10-01 23:04:44 +0530

    update/profile change in skills

--- src/scripts/skillRatings.js
+++ src/scripts/skillRatings.js
@@ -3,7 +3,7 @@ exports.skillRecords =
         backend         : ['NodeJs', 'ExpressJS', 'NestJS', 'Socket.IO'],
         events          : ['Kafka', 'Redis BullMQ', 'Rabbit MQ'],
         frontend        : ['ReactJS', 'Redux', 'Bootstrap5.0', 'MaterialUI 5.0', 'ANT Design'],
-        database        : ['Mongo DB', 'Redis', 'MySQL/SQL', 'Postgres', 'KV'],
+        database        : ['Mongo DB', 'Postgres', 'Elastic Search', 'Redis', 'MySQL/SQL', 'KV'],
         tools           : ['Git', 'Jira Board', 'VS Code', "Vim Editor"],
         languages       : ['Javascript', 'Typescript', 'HTML/CSS', 'Python', 'C/C++'],
         technologies    : ['AWS-', 'S3','SES', 'SQS', "SNS", "Congnito", "tus.io", "Payment Gateways", "WhatsApp API"]
